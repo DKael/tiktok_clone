0f62647218667a327f50686bb9934fb4aa3499e9
DMKael <hyungdki@student.42seoul.kr>
2024-01-08 06:11:22 +0900

    강의 6.6강까지 내용

---
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/stf_screen.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    // const Center(
+    //   child: Text('Home'),
+    // ),
+    // const Center(
+    //   child: Text('Discover'),
+    // ),
+    // const Center(
+    //   child: Text(''),
+    // ),
+    // const Center(
+    //   child: Text('Inbox'),
+    // ),
+    // const Center(
+    //   child: Text('Profile'),
+    // )
+    StfScreen(key: GlobalKey()),
+    StfScreen(key: GlobalKey()),
+    Container(),
+    StfScreen(key: GlobalKey()),
+    StfScreen(key: GlobalKey()),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            Sizes.size12,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                label: "Home",
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                label: "Discover",
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                label: "Inbox",
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                label: "Profile",
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    // CupertinoTabScaffold(
+    //   tabBar: CupertinoTabBar(
+    //     items: const [
+    //       BottomNavigationBarItem(
+    //         icon: Icon(CupertinoIcons.house),
+    //         label: "Home",
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: FaIcon(CupertinoIcons.search),
+    //         label: "Search",
+    //       ),
+    //     ],
+    //   ),
+    //   tabBuilder: (context, index) => screens[index],
+    // );
+
+    // Scaffold(
+    //   body: screens[_selectedIndex],
+    //   bottomNavigationBar:
+    // NavigationBar(
+    //   selectedIndex: _selectedIndex,
+    //   onDestinationSelected: _onTap,
+    //   labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+    //   destinations: const [
+    //     NavigationDestination(
+    //       icon: FaIcon(
+    //         FontAwesomeIcons.house,
+    //         color: Colors.white,
+    //       ),
+    //       label: 'Home',
+    //     ),
+    //     NavigationDestination(
+    //       icon: FaIcon(
+    //         FontAwesomeIcons.magnifyingGlass,
+    //         color: Colors.amber,
+    //       ),
+    //       label: 'Search',
+    //     )
+    //   ],
+    // ),
+
+    // BottomNavigationBar(
+    //   currentIndex: _selectedIndex,
+    //   onTap: _onTap,
+    //   selectedItemColor: Theme.of(context).primaryColor,
+    //   items: const [
+    //     BottomNavigationBarItem(
+    //       icon: FaIcon(FontAwesomeIcons.house),
+    //       label: "Home",
+    //       tooltip: "what",
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //       label: "Search",
+    //       tooltip: "what",
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //       label: "Search",
+    //       tooltip: "what",
+    //       backgroundColor: Colors.teal,
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //       label: "Search",
+    //       tooltip: "what",
+    //       backgroundColor: Colors.yellow,
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //       label: "Search",
+    //       tooltip: "what",
+    //       backgroundColor: Colors.blue,
+    //     )
+    //   ],
+    // ),
+  }
+}
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  final IconData selectedIcon;
+  final IconData icon;
+  final bool isSelected;
+  final String label;
+  final Function onTap;
+
+  const NavTab(
+      {super.key,
+      required this.icon,
+      required this.label,
+      required this.isSelected,
+      required this.onTap,
+      required this.selectedIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: AnimatedOpacity(
+          duration: const Duration(
+            milliseconds: 300,
+          ),
+          opacity: isSelected ? 1 : 0.6,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(
+                isSelected ? selectedIcon : icon,
+                color: Colors.white,
+              ),
+              Gaps.v5,
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
