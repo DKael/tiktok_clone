f283928e34f3b8f317ea89491af5e8c65d23b598
DMKael <hyungdki@student.42seoul.kr>
2024-01-09 20:24:56 +0900

    .

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/stf_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -14,28 +16,28 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    // const Center(
-    //   child: Text('Home'),
-    // ),
-    // const Center(
-    //   child: Text('Discover'),
-    // ),
-    // const Center(
-    //   child: Text(''),
-    // ),
-    // const Center(
-    //   child: Text('Inbox'),
-    // ),
-    // const Center(
-    //   child: Text('Profile'),
-    // )
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-    Container(),
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-  ];
+  // final screens = [
+  //   // const Center(
+  //   //   child: Text('Home'),
+  //   // ),
+  //   // const Center(
+  //   //   child: Text('Discover'),
+  //   // ),
+  //   // const Center(
+  //   //   child: Text(''),
+  //   // ),
+  //   // const Center(
+  //   //   child: Text('Inbox'),
+  //   // ),
+  //   // const Center(
+  //   //   child: Text('Profile'),
+  //   // )
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  //   Container(),
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  // ];
 
   void _onTap(int index) {
     setState(() {
@@ -43,10 +45,43 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     });
   }
 
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text("Record video"),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens.elementAt(_selectedIndex),
+      //body: screens.elementAt(_selectedIndex),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const StfScreen(),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
@@ -70,6 +105,11 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 isSelected: _selectedIndex == 1,
                 onTap: () => _onTap(1),
               ),
+              Gaps.h24,
+              GestureDetector(
+                child: PostVideoButton(onTap: _onPostVideoButtonTap),
+              ),
+              Gaps.h24,
               NavTab(
                 icon: FontAwesomeIcons.message,
                 selectedIcon: FontAwesomeIcons.solidMessage,
